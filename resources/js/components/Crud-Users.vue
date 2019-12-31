eb05f2a00c4770ba8077274fd3d68982149a4087
hamzaoui_idriss <hi_hamzaoui@esi.dz>
2019-12-31 18:50:37 +0100

    finished-back

---
+++ resources/js/components/Crud-Users.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Crud-Users"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Crud-students.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Crud-students"
+    }
+</script>
+
+<style scoped>
+
+</style>
